69bb77a0dd02c7b2327a9dcbf17159b06c486611
xunjijiang <329453626x@gmail.com>
2024-06-14 20:49:09 +0800

    feat: export from index

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,13 +1,3 @@
-type Shared = {
-  name: string;
-  age: number;
-  sayHello: () => void;
-};
-
-export const shared: Shared = {
-  name: 'shared',
-  age: 18,
-  sayHello: () => {
-    console.log('Hello, I am shared');
-  },
-};
+export { makeMap } from './makeMap';
+export * from './domTagConfig';
+export * from './domAttrConfig';
