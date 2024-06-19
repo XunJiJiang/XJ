72cb5ec7872a21b11a5516300ddde5bf81ecd824
xunjijiang <329453626x@gmail.com>
2024-06-19 16:14:33 +0800

    feat(shared): uniqueKey support big numbers

--- packages/shared/src/uniqueKey.ts
+++ packages/shared/src/uniqueKey.ts
@@ -1,6 +1,16 @@
 export type UniqueKeyTemplate = `${string}#{key}${string}`
 
-const templateMap = new Map<string, number>()
+const templateMap = new Map<string, string>()
+
+const countAdd = (count: `${number}`): `${number}` => {
+  const last = count[count.length - 1]
+  if (last === '9') {
+    const newCount = countAdd(count.slice(0, -1) as `${number}`)
+    return `${newCount}0` as `${number}`
+  } else {
+    return `${count.slice(0, -1)}${Number(last) + 1}` as `${number}`
+  }
+}
 
 export const defaultUniqueKeyTemplate = '__:#{key}:__'
 
@@ -8,11 +18,11 @@ export const getUniqueKey = (
   template: UniqueKeyTemplate = defaultUniqueKeyTemplate,
 ): string => {
   if (templateMap.has(template)) {
-    const count = templateMap.get(template) as number
-    templateMap.set(template, count + 1)
-    return `${template.replace('#{key}', String(count))}`
+    const count = templateMap.get(template) as `${number}`
+    templateMap.set(template, countAdd(count))
+    return `${template.replace('#{key}', count)}`
   } else {
-    templateMap.set(template, 1)
+    templateMap.set(template, '0')
     return `${template.replace('#{key}', '0')}`
   }
 }
