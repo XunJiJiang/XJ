4ff78d2206f28c020c2ee352c720fce3cbbc11a7
xunjijiang <329453626x@gmail.com>
2024-12-20 19:33:52 +0800

    chore(husky)：优化错误提示

--- .husky/pre-commit.js
+++ .husky/pre-commit.js
@@ -1,33 +1,50 @@
-import { execSync } from 'child_process'
+import { exec } from 'node:child_process'
 
-try {
-  const stagedFiles = execSync(
-    'git diff --cached --name-only --diff-filter=ACM'
+new Promise((resolve, reject) => {
+  exec(
+    'git diff --cached --name-only --diff-filter=ACM',
+    (error, stdout, stderr) => {
+      if (error) {
+        reject(`git diff 获取失败\n ${error.message}`)
+      }
+      if (stderr) {
+        reject(`git diff 获取失败\n ${stderr}`)
+      }
+      resolve(
+        stdout
+          .toString()
+          .split('\n')
+          .filter((file) =>
+            /^(packages|scripts|tests)\/.*|.*\.(ts|mjs|json|md)$/.test(file)
+          )
+      )
+    }
   )
-    .toString()
-    .split('\n')
-    .filter((file) =>
-      /^(packages|scripts|tests)\/.*|.*\.(ts|mjs|json|md)$/.test(file)
-    )
-
-  if (stagedFiles.length > 0) {
-    const output = execSync(`prettier --check ${stagedFiles.join(' ')}`, {
-      encoding: 'utf-8'
-    })
-
-    if (/warn/i.test(output)) {
-      execSync(`prettier --write ${stagedFiles.join(' ')}`)
-      console.log('代码格式化检查包含警告，提交已取消。')
-      process.exit(1)
+})
+  .then((stagedFiles) => {
+    if (stagedFiles.length > 0) {
+      return new Promise((resolve, reject) => {
+        exec(
+          `prettier --check ${stagedFiles.join(' ')}`,
+          {
+            encoding: 'utf-8'
+          },
+          (error, _stdout, stderr) => {
+            if (error) {
+              reject(
+                `prettier 格式化检查失败\n ${error.message.replace(`Command failed: prettier --check`, `尝试运行 prettier --write`)}`
+              )
+            }
+            if (stderr) {
+              reject(`prettier 格式化检查失败\n ${stderr}`)
+            }
+            resolve()
+          }
+        )
+      })
     }
-
-    if (/error/i.test(output)) {
-      execSync(`prettier --write ${stagedFiles.join(' ')}`)
-      console.log('代码格式化检查包含错误，提交已取消。')
-      process.exit(1)
-    }
-  }
-} catch (error) {
-  console.error('pre-commit 脚本执行失败:', error)
-  process.exit(1)
-}
+  })
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
